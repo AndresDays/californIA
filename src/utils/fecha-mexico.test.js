e53c371c3fc67583b83a650a17fa7c039d89d179
AndresDays <119088908+AndresDays@users.noreply.github.com>
2026-08-18 16:47:29 -0600

    Fix hora local

--- src/utils/fecha-mexico.test.js
+++ src/utils/fecha-mexico.test.js
@@ -1,4 +1,7 @@
-import { crearRangoFechaMexico } from './fecha-mexico';
+import {
+	crearRangoFechaMexico,
+	formatearFechaHoraMexicoLocal,
+} from './fecha-mexico';
 
 describe('crearRangoFechaMexico', () => {
 	it('incluye toda la jornada local aunque termine en el siguiente día UTC', () => {
@@ -6,5 +9,12 @@ describe('crearRangoFechaMexico', () => {
 			inicio: '2026-08-06T00:00:00-06:00',
 			fin: '2026-08-07T00:00:00-06:00',
 		});
+	});
+});
+
+describe('formatearFechaHoraMexicoLocal', () => {
+	it('convierte un instante UTC nocturno a fecha y hora civil de México sin zona', () => {
+		expect(formatearFechaHoraMexicoLocal(new Date('2026-08-18T00:27:00.000Z')))
+			.toBe('2026-08-17T18:27:00');
 	});
 });
